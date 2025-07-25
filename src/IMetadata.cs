80e30eaa37f77f9a8f2871ad5f9e1986a6f8e3de
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-07-25 11:05:34 +0200

    feat: extend metadata abstractions with tag update and fluent builder APIs
    - Updated IMetadata interface:
      - Added methods for removing tags (`WithoutTag`, `WithoutTags`)
      - Added bulk update methods for adding/replacing multiple tags (`WithTags`) and removing multiple tags (`WithoutTags`)

--- src/IMetadata.cs
+++ src/IMetadata.cs
@@ -9,19 +9,13 @@ namespace Zentient.Abstractions
     /// </summary>
     public interface IMetadata
     {
-        /// <summary>
-        /// Gets the set of available tag keys.
-        /// </summary>
+        /// <summary>Gets the set of available tag keys.</summary>
         IReadOnlyCollection<string> Keys { get; }
 
-        /// <summary>
-        /// Gets the set of available tag values.
-        /// </summary>
+        /// <summary>Gets the set of available tag values.</summary>
         IReadOnlyCollection<object> Values { get; }
 
-        /// <summary>
-        /// Attempts to retrieve a metadata tag of type <typeparamref name="TValue"/> by key.
-        /// </summary>
+        /// <summary>Attempts to retrieve a metadata tag of type <typeparamref name="TValue"/> by key.</summary>
         /// <typeparam name="TValue">The type of the tag value.</typeparam>
         /// <param name="key">The key for the tag.</param>
         /// <param name="value">When this method returns, contains the tag value if found; otherwise, <see langword="null"/>.</param>
@@ -31,13 +25,30 @@ namespace Zentient.Abstractions
         /// </returns>
         bool TryGetTag<TValue>(string key, out TValue? value);
 
-        /// <summary>
-        /// Returns a new metadata instance with the given tag added or replaced.
-        /// </summary>
+        /// <summary>Returns a new metadata instance with the given tag added or replaced.</summary>
         /// <typeparam name="TValue">The type of the tag value.</typeparam>
         /// <param name="key">The key for the tag.</param>
         /// <param name="value">The value to associate with the key.</param>
         /// <returns>A new <see cref="IMetadata"/> instance with the updated tag.</returns>
         IMetadata WithTag<TValue>(string key, TValue value);
+
+        /// <summary>Returns a new metadata instance without the given tag added or replaced.</summary>
+        /// <typeparam name="TValue">The type of the tag value.</typeparam>
+        /// <param name="key">The key for the tag.</param>
+        /// <param name="value">The value to associate with the key.</param>
+        /// <returns>A new <see cref="IMetadata"/> instance with the updated tag.</returns>
+        IMetadata WithoutTag<TValue>(string key, TValue value);
+
+        // Multiple update tag methods: WithTags and WithoutTags
+
+        /// <summary>Returns a new metadata instance with the given tags added or replaced.</summary>
+        /// <param name="tags">A dictionary of key-value pairs to add or replace.</param>
+        /// <returns>A new <see cref="IMetadata"/> instance with the updated tags.</returns>
+        IMetadata WithTags(IDictionary<string, object> tags);
+
+        /// <summary>Returns a new metadata instance without the given tags.</summary>
+        /// <param name="keys">A collection of keys to remove.</param>
+        /// <returns>A new <see cref="IMetadata"/> instance with the specified tags removed.</returns>
+        IMetadata WithoutTags(IEnumerable<string> keys);
     }
 }
